5dda3a1dc201acad35fe7e057572de9691659d3f
Arun Pratap Singh <arun.singh1@etmoney.com>
2023-01-29 15:18:12 +0530

    Minor test comment added

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
-
+//Some changes
 export default function App() {
   return (
     <View style={styles.container}>
